20278aa88f0963d3ff43200c8d9fb198eb4d927a
begin0071 <m.khaki610@gmail.com>
2023-02-13 19:10:52 -0600

    finalizing

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,50 @@
-DROP DATABASE IF EXISTS employee_tracker;
-CREATE DATABASE employee_tracker;
+ DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employee_tracker;
+USE employees_db;
 
 CREATE TABLE department (
-  department_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  names VARCHAR(30),
+  PRIMARY KEY (id)
 );
+
+INSERT INTO department (id, names)
+VALUES (1, "Seafood");
+INSERT INTO department (id, names)
+VALUES (2, "Kosher");
+INSERT INTO department (id, names)
+VALUES (3, "Nuts");
 
 CREATE TABLE role (
-  role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  role_title VARCHAR(30) NOT NULL,
-  role_salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES department(department_id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Food Handler", 45000, 1);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Kosher Distribution", 50000, 2);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Nuts Delivery", 75000, 3);
+
 CREATE TABLE employee (
-  employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  employee_first_name VARCHAR(30) NOT NULL,
-  employee_last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES role(role_id),
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
   manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(employee_id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mugees", "Khaki", 3, NULL);
---
+++ index.js
@@ -0,0 +1,143 @@
+const inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  PORT: "3306",
+  user: "root",
+  password: "passkeyword",
+  database: "employees_db"
+});
+
+con.connect(function (error) {
+  if (error) throw error;
+  console.log("You are now connected to the employees_db database.");
+});
+startQuestions();
+function startQuestions() {
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit'],
+        name: 'action'
+      },
+    ])
+
+    .then(function (responses) {
+      switch (responses.action) {
+        case "View all Departments":
+          con.query("SELECT * FROM department", function (error, result, fields) {
+            if (error) throw error;
+            console.table(result);
+            startQuestions();
+          });
+          break;
+
+        case "View all Roles":
+          con.query("SELECT * FROM role", function (error, result, fields) {
+            if (error) throw error;
+            console.table(result);
+            startQuestions();
+          });
+          break;
+
+        case "View all Employees":
+          con.query("SELECT * FROM employee", function (error, result, fields) {
+            if (error) throw error;
+            console.table(result);
+            startQuestions();
+          });
+          break;
+
+        case "Add a Department":
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                message: 'What is the name of the Department?',
+                name: 'deptName'
+              }
+            ])
+            .then(function (responses) {
+              conn.query("INSERT INTO department (names) VALUES('" + responses.deptName + "')");
+              startQuestions();
+            });
+
+          break;
+
+        case "Add a Role":
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                message: 'What is the role/title?',
+                name: 'roleTitle'
+              },
+              {
+                type: 'input',
+                message: 'What is the salary?',
+                name: 'salary'
+              },
+              {
+                type: 'input',
+                message: 'What is the department id?',
+                name: 'deptId'
+              }
+            ])
+            .then(function (responses) {
+              con.query("INSERT INTO role (title, salary, department_id) VALUES('" + responses.roleTitle + "', '" + responses.salary + "', " + responses.deptId + ")");
+              startQuestions();
+            });
+          break;
+
+        case "Add an Employee":
+          inquirer
+            .prompt([
+              {
+                type: 'input',
+                message: 'What is the first name',
+                name: 'firstName'
+              },
+              {
+                type: 'input',
+                message: 'What is the last name',
+                name: 'lastName'
+              },
+              {
+                type: 'input',
+                message: 'What is the role id',
+                name: 'roleId'
+              },
+
+            ])
+            .then(function (answers) {
+              var sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('" + answers.first_name + "', '" + answers.last_name + "', " + answers.role_id + ", " + answers.manager_id + ")";
+              con.query(sql);
+              askQuestions();
+            });
+          break;
+        case "Update an Employee Role":
+          inquirer
+            .prompt([
+              {
+                input: 'input',
+                message: 'Which employee would you like to update (enter id)?',
+                name: 'updateEmployee'
+              },
+              {
+                input: 'input',
+                message: 'What will be their new role (enter role id)?',
+                name: 'updateId'
+              },
+            ])
+            .then(function (answers) {
+              con.query("UPDATE employee SET role_id = " + answers.updateId + " WHERE id = " + answers.updateEmployee);
+              askQuestions();
+            })
+          break;
+      }
+    });
+};
